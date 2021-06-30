33c43d8d61b5880ae14d90c2edf162254861c8de
good665 <good66591@gmail.com>
2021-07-01 07:41:12 +0900

    flutter

---
+++ lib/main6.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_project/src/radial_menu.dart';
+import 'package:flutter_project/src/radial_menu_item.dart';
+
+// void main() => runApp(MyApp());
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     //final wordPair = WordPair.random();
+//     return MaterialApp(
+//         title: 'Welcome to Flutter',
+//         theme: ThemeData(
+//           // Add the 3 lines from here...
+//           primaryColor: Colors.white,
+//         ),
+//         home: new Scaffold(
+//           body: new Center(
+//             child: new RadialMenu(
+//               items: <RadialMenuItem<int>>[
+//                 const RadialMenuItem<int>(
+//                   value: 1,
+//                   child: const Icon(Icons.add),
+//                 ),
+//                 const RadialMenuItem<int>(
+//                   value: -1,
+//                   size: 30,
+//                   child: const Icon(Icons.remove),
+//                 ),
+//                 const RadialMenuItem<int>(
+//                   value: -1,
+//                   child: const Icon(Icons.remove),
+//                 ),
+//                 const RadialMenuItem<int>(
+//                   value: -1,
+//                   child: const Icon(Icons.remove),
+//                 )
+//               ],
+//               radius: 100.0,
+//               onSelected: print,
+//             ),
+//           ),
+//         ));
+//   }
+// }
